c30c8597f11ab59d120038ad0a5ebbe4796ad0e7
zanuka <git@zanuka.com>
2019-01-06 22:18:18 +1300

    adds button / container

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,32 @@ void main() {
 }
 
 class EarthViz extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EarthViz'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/earth_one.jpg'),
-              Text('Earth')
-            ],
+          appBar: AppBar(
+            title: Text('EarthViz'),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Item'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/earth_one.jpg'),
+                    Text('Earth')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
