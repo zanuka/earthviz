9ffe6d10681e259bb7ecccd4aafd4513b0289c56
zanuka <git@zanuka.com>
2019-04-21 00:07:41 +1200

    adds stateful widget pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './data_manager.dart';
+import 'package:earthviz/src/app.dart';
 
-void main() => runApp(EarthViz());
-
-class EarthViz extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('earth viz');
-          },
-        ),
-        appBar: AppBar(
-          title: Text('EarthViz'),
-        ),
-        body: DataManager('Air Temp'),
-      ),
-    );
-  }
+void main() {
+  runApp(App());
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,38 @@
+// Import flutter helper library
+import 'package:flutter/material.dart';
+
+import 'package:earthviz/src/services/data_manager.dart';
+
+class App extends StatefulWidget {
+  createState() => AppState();
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.indigo,
+          child: Icon(
+            Icons.terrain,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            setState(() {
+              setState(() {
+                counter += 1;
+              });
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text('EarthViz'),
+        ),
+        // body: DataManager('Air Temp'),
+      ),
+    );
+  }
+}
