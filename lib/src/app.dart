32e12d56e1e6a45a3add1263aaee327438401c6b
zanuka <git@zanuka.com>
2019-08-12 10:07:52 +1200

    adds image model

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
-// Import flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'models/image_model.dart';
+import 'dart:convert';
 
 class App extends StatefulWidget {
   createState() => AppState();
@@ -7,6 +9,29 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  String base = 'http://jsonplaceholder.typicode.com';
+  String path = 'photos';
+  String url = '';
+
+  void fetchImage() async {
+    counter += 1;
+    url = '$base/$path/$counter';
+
+    http.Response response = await http.get(url);
+    if (response.body != null) {
+      dynamic data = json.decode(response.body);
+      var imageModel = ImageModel.fromJson(data);
+      if (data['success'] == 1) {
+        return data['result'];
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
 
   Widget build(context) {
     return MaterialApp(
@@ -18,13 +43,7 @@ class AppState extends State<App> {
             Icons.terrain,
             color: Colors.white,
           ),
-          onPressed: () {
-            setState(() {
-              setState(() {
-                counter += 1;
-              });
-            });
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text('EarthViz'),
